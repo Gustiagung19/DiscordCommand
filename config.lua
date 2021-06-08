55c166228a261cbdd8a2722288eb604affdbcbc9
Gustiagung19 <75777108+Gustiagung19@users.noreply.github.com>
2021-06-09 00:07:19 +0800

    Delete config.lua

--- config.lua
+++
@@ -1,9 +0,0 @@
-Config = {}
-Config.ESX = false
-Config.BotToken = ""
-Config.WebHook = ""
-Config.ChannelID = ""
-Config.ReplyUserName = ""
-Config.AvatarURL = ""
-Config.Prefix = "!"
-Config.WaitEveryTick = 4000  -- ITS MS
